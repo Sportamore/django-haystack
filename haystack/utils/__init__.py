dc04cf60c2501a6b73a806472db8f6f8fa2b9fa3
Henrik Lindgren <henrik.lindgren@sportamore.se>
2015-12-10 13:58:49 +0100

    flipping importlib import try-catch so it would still work with older pythons and not trigger depreciated warnings on later ones.

--- haystack/utils/__init__.py
+++ haystack/utils/__init__.py
@@ -11,9 +11,10 @@ from haystack.constants import ID, DJANGO_CT, DJANGO_ID
 from haystack.utils.highlighting import Highlighter
 
 try:
-    from django.utils import importlib
-except ImportError:
     import importlib
+except ImportError:
+    from django.utils import importlib
+
 
 IDENTIFIER_REGEX = re.compile('^[\w\d_]+\.[\w\d_]+\.\d+$')
 
